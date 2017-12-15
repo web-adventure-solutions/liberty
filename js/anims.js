1fa70ae2697866d4d15a0b56f3fc0d41525ba181
Iterl0 <Iterl054@gmail.com>
2017-12-15 16:16:28 +0200

    header anim var 2.1 beta test

--- js/anims.js
+++ js/anims.js
@@ -1,56 +1,64 @@
 $(document).ready(function () {
-    // $("#stripe").removeClass('stripe-anim-start');
-    // $("#stripe").addClass('stripe-anim-end', 3000);
 
-    $('header .jumbotron h2').css({'left': '-60px'});
-    // $('header .jumbotron .jumbotron-footer').css({'right' : '-60px'});
-    $('header .jumbotron-footer-list-item').css({'right': '-60px'});
+// uncomment to enable on reload
+    $("#stripe").removeClass('stripe-anim-start');
+    $("#stripe").addClass('stripe-anim-end', 3000);
+    $('header p.opacity-0').animate({'opacity': 1}, 1000);
+    $('header .jumbotron h2, .jumbotron .parallelogram')
+        .animate({'left': '0', 'opacity': '1'}, {
+            duration: 1000,
+            complete: function () {
+                func(1, duration)
+            }
+        });
+
+    function func(i, duration) {
+        $(jumbotronFooterListItem + ':nth-child('+ i +')')
+            .animate({'right': 0, 'opacity': 1 }, {
+                duration: duration,
+                complete: function () {
+                    if (i<=3) {
+                        i++;
+                        func(i, duration);
+                    }
+                }
+            })
+    }
+
+
+
+    var duration = 800;
+    var jumbotronFooterListItem = 'header .jumbotron-footer-list-item';
 
     $('#btn-anim-start').click(function () {
         $('header .jumbotron h2, .jumbotron .parallelogram')
             .animate({'left': '0', 'opacity': '1'}, {
                 duration: 1000,
                 complete: function () {
-                    animateJumbotronFooter(800)
+                    func(1, duration)
                 }
             });
 
-        $('header .jumbotron .jumbotron-footer').animate({'right': 0}, 1000);
+        function func(i, duration) {
+            $(jumbotronFooterListItem + ':nth-child('+ i +')')
+                .animate({'right': 0, 'opacity': 1 }, {
+                    duration: duration,
+                    complete: function () {
+                        if (i<=3) {
+                            i++;
+                            func(i, duration);
+                        }
+                    }
+                })
+        }
+
+
         $("#stripe").removeClass('stripe-anim-end', 1, function () {
             $(this).addClass('stripe-anim-start', 1, function () {
                 $(this).addClass('stripe-anim-end', 3000, 'easeOutQuint');
             });
         });
         $('header p.opacity-0').animate({'opacity': 1}, 1000);
-
-        // for (i=1; i <= 3; i++) {
-        //     $('header .jumbotron-footer-list-item:nth-child(' + i + ')')
-        //         .animate({'right' : 0}, {duration: 1000, queue: false})
-        //         .animate({'opacity': 1}, {duration: 1000, queue: false});
-        // }
-
-
-
-        function animateJumbotronFooter(duration) {
-            var jumbotronFooterListItem = 'header .jumbotron-footer-list-item';
-            $(jumbotronFooterListItem + ':nth-child(1)')
-                .animate({'right': 0, 'opacity': 1 }, {
-                    duration: duration,
-                    complete: function () {
-                        $(jumbotronFooterListItem + ':nth-child(2)')
-                            .animate({'right': 0, 'opacity': 1}, {
-                                duration: duration,
-                                complete: function () {
-                                    $(jumbotronFooterListItem + ':nth-child(3)')
-                                        .animate({'right': 0, 'opacity': 1}, {duration: duration})
-                                }
-                            })
-                    }
-                })
-
-        }
-
-
 
     });
 
