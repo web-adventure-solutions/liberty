c265b819b4d1cca566e8a748c07f17b084760511
Iterl0 <Iterl054@gmail.com>
2018-01-17 01:52:07 +0200

    before trying scss

--- js/anims.js
+++ js/anims.js
@@ -1,33 +1,48 @@
 $(document).ready(function () {
-
     var duration = 800;
     var jumbotronFooterListItem = 'header .jumbotron-footer-list-item';
-
+    var stripe = $("#stripe");
 // uncomment to enable on reload
-//     $("#stripe").removeClass('stripe-anim-start');
-//     $("#stripe").addClass('stripe-anim-end', 3000);
-//     $('header p.opacity-0').animate({'opacity': 1}, 1000);
-//     $('header .jumbotron h2, .jumbotron .parallelogram')
-//         .animate({'left': '0', 'opacity': '1'}, {
-//             duration: 1000,
-//             complete: function () {
-//                 func(1, duration)
-//             }
-//         });
-//
-//     function func(i, duration) {
-//         $(jumbotronFooterListItem + ':nth-child('+ i +')')
-//             .animate({'right': 0, 'opacity': 1 }, {
-//                 duration: duration,
-//                 complete: function () {
-//                     if (i<=3) {
-//                         i++;
-//                         func(i, duration);
-//                     }
-//                 }
-//             })
-//     }
 
+    // see if mobile device is in horizontal position
+    mobView = (winW < 768 && winH < 1024);
+    posVertical = (winW < winH);
+
+    console.log('mobile : ' + mobView);
+    console.log('vertical : ' + posVertical);
+
+
+    if (mobView && posVertical) {
+        stripe
+            .animate({'right' : '61vh'}, { duration : 3000, queue : false})
+            .animate({'-webkit-mask-position-x' : '20px'}, { duration : 3000, queue : false});
+    } else if (!mobView) {
+        stripe.addClass('stripe-anim-end', 3000)
+    }
+
+
+
+
+    $('header .jumbotron-content').animate({'opacity': 1}, 1000);
+    $('header .jumbotron h2, .jumbotron .parallelogram')
+        .animate({'left': '0', 'opacity': '1'}, {
+            duration: 1000,
+            complete: function () {
+                func(1, duration)
+            }
+        });
+    function func(i, duration) {
+        $(jumbotronFooterListItem + ':nth-child('+ i +')')
+            .animate({'right': 0, 'opacity': 1 }, {
+                duration: duration,
+                complete: function () {
+                    if (i<=3) {
+                        i++;
+                        func(i, duration);
+                    }
+                }
+            })
+    }
     $(document).keyup(function (e) {
         if (e.keyCode === 65) {
             triggerAnim();
@@ -35,7 +50,6 @@ $(document).ready(function () {
             resetAnim();
         }
     });
-
     $('#btn-anim-start').click(function () {
         triggerAnim();
     });
@@ -43,8 +57,6 @@ $(document).ready(function () {
     $('#btn-anim-reset').click(function () {
         resetAnim();
     });
-
-
     function triggerAnim() {
         $('header .jumbotron h2, .jumbotron .parallelogram')
             .animate({'left': '0', 'opacity': '1'}, {
@@ -66,7 +78,6 @@ $(document).ready(function () {
                     }
                 })
         }
-
 
         $("#stripe").removeClass('stripe-anim-end', 1, function () {
             $(this).addClass('stripe-anim-start', 1, function () {
